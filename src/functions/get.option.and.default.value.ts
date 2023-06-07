177eb04cfb9973a988c32c25355ec5101674605a
cope <predrag@stojadinovic.net>
2023-06-07 12:12:11 +0200

    test: adding more tests

--- src/functions/get.option.and.default.value.ts
+++ src/functions/get.option.and.default.value.ts
@@ -2,12 +2,12 @@
 
 export const getOptionAndDefaultValue = (config: string): {option: string; defaultValue: string} => {
 	let defaultValue;
-	let option = config.substring(1).substring(-1).slice(0, -1);
+	let option = config.substring(1).substring(-1).slice(0, -1).trim();
 
 	// option has default value
 	if (option.indexOf('=') >= 0) {
-		defaultValue = option.substring(option.indexOf('=') + 1).slice(0, -1);
-		option = option.replace('=' + defaultValue, '');
+		defaultValue = option.substring(option.indexOf('=') + 1).trim();
+		option = option.replace('=' + defaultValue, '').trim();
 	}
 
 	return {option, defaultValue};
--- test/functions/get.option.and.default.value.spec.ts
+++ test/functions/get.option.and.default.value.spec.ts
@@ -10,5 +10,31 @@ describe('get.option.and.default.value tests', () => {
 		expect(getOptionAndDefaultValue).to.be.a('function');
 	});
 
-	it('should be implemented');
+	it('getOptionAndDefaultValue works', () => {
+		const signature = 'some_command {-f, --foo <foo>} {-b, --boo <boo>}';
+		const options: string[] = signature.match(/{([^}]*)}/g);
+		expect(options).to.have.length(2);
+
+		const {option: option1, defaultValue: defaultValue1} = getOptionAndDefaultValue(options[0]);
+		expect(option1).to.equal('-f, --foo <foo>');
+		expect(defaultValue1).to.be.undefined;
+
+		const {option: option2, defaultValue: defaultValue2} = getOptionAndDefaultValue(options[1]);
+		expect(option2).to.equal('-b, --boo <boo>');
+		expect(defaultValue2).to.be.undefined;
+	});
+
+	it('getOptionAndDefaultValue works with default value', () => {
+		const signature = 'some_command {-f, --foo <foo>=yei} {-b, --boo <boo>}';
+		const options: string[] = signature.match(/{([^}]*)}/g);
+		expect(options).to.have.length(2);
+
+		const {option: option1, defaultValue: defaultValue1} = getOptionAndDefaultValue(options[0]);
+		expect(option1).to.equal('-f, --foo <foo>');
+		expect(defaultValue1).to.equal('yei');
+
+		const {option: option2, defaultValue: defaultValue2} = getOptionAndDefaultValue(options[1]);
+		expect(option2).to.equal('-b, --boo <boo>');
+		expect(defaultValue2).to.be.undefined;
+	});
 });
